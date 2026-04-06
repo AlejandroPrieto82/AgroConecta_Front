34499299d30cd887d8041d860538ccf7cd2ec054
Alejandro Prieto <samuel.prieto-r@mail.escuelaing.edu.co>
2026-04-05 22:07:10 -0500

    Seguimos en lo mismo, sorry

--- src/context/UserProvider.tsx
+++ src/context/UserProvider.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import type { ReactNode } from "react";
 import { UserContext } from "./UserContext";
 import type { User } from "./UserContext";
